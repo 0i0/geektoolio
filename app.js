823c30a111122c3573bb402772da9118f564d684
Lior hakim <leo@Liors-MacBook-Pro.local>
2017-10-08 06:15:22 +0300

    formating and requirejs on FE

--- app.js
+++ app.js
@@ -1,104 +1,111 @@
-var child_process = require('child_process');
-var readline      = require('readline');
-var os            = require('os');
-var exec0         = child_process.exec;
-const si          = require('systeminformation');
-var smc           = require('smc');
-var request       = require('request');
-var nowplaying = require("nowplaying");
-var express = require('express')
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var child_process = require('child_process')
+var os            = require('os')
+  , exec0         = child_process.exec
+  , si            = require('systeminformation')
+  , smc           = require('smc')
+  , request       = require('request')
+  , nowplaying    = require('nowplaying')
+  , express       = require('express')
 
-app.use(express.static(__dirname + '/public'));
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
-app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'))
+
+app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
 
 app.get('/',function(req,res){
-  res.render('index');
+  res.render('index')
 })
 app.get('/test',function(req,res){
-  res.render('test');
+  res.render('test')
 })
 app.get('/pcpu', function (req, res) {
-  var cpus_start = os.cpus();
+  var cpus_start = os.cpus()
   setTimeout(function(){
-    var cpus_end = os.cpus();
-    pcpu = [];
+    var cpus_end = os.cpus()
+    var pcpu = []
     for(var i = 0, len = cpus_end.length; i < len; i++) {
-        var cpu_start = cpus_start[i],
-            cpu_end =   cpus_end[i],
-            total = 0;
+      var cpu_start = cpus_start[i]
+        , cpu_end =   cpus_end[i]
+        , total = 0
 
-        for(var type in cpu_start.times) {
-            total += cpu_end.times[type]-cpu_start.times[type];
-        }
-        cpu_user = cpu_end.times.user-cpu_start.times.user
-        cpu_sys  = cpu_end.times.sys-cpu_start.times.sys
-        pcpu[i] = Math.round(100 * (cpu_user+cpu_sys)/ total)
+      for(var type in cpu_start.times) {
+        total += cpu_end.times[type]-cpu_start.times[type]
+      }
+      var cpu_user = cpu_end.times.user-cpu_start.times.user
+        , cpu_sys  = cpu_end.times.sys-cpu_start.times.sys
+      pcpu[i] = Math.round(100 * (cpu_user+cpu_sys)/ total)
         
     }
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'application/json')
     res.send(JSON.stringify(pcpu))
   },1500)
 
 })
 app.get('/cputemp', function (req, res) {
-    res.send(smc.get('TC0P').toString())
+  res.send(smc.get('TC0P').toString())
 })
 app.get('/mem', function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({total:os.totalmem()
-                          ,free:os.freemem()
-                          }))
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify(
+    {
+      total:os.totalmem(),
+      free:os.freemem()
+    }
+  ))
 })
 app.get('/net', function (req, res) {
   si.networkInterfaceDefault(function(interface){
     si.networkStats(interface, function(data) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({rx:Math.round(data.rx_sec/8/1024*100)/100
-                          ,tx:Math.round(data.tx_sec/8/1024*100)/100
-                          }))
+      res.setHeader('Content-Type', 'application/json')
+      res.send(JSON.stringify(
+        {
+          rx:Math.round(data.rx_sec/8/1024*100)/100,
+          tx:Math.round(data.tx_sec/8/1024*100)/100
+        }
+      ))
     })
   })
 })
 app.get('/ps', function (req, res) {
   function cb(error, stdout, stderr) {
-    str = stdout.replace(/:/g,'],[')
+    if(stderr) return
+    var str = stdout.replace(/:/g,'],[')
     str = str.replace(/</g,'"')
     str = '[['+str+']]'
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'application/json')
     res.send(str)
-  };
-  exec0("ps -Ao pid,%cpu,%mem,comm |sort -nrk 2 | head -n 5 | awk '{gsub(\"(.+/)\",\"\",$4);print \"<\"substr($4,1,13)\"<\"\",\"$1\",\"$2\",\"$3\":\"}'",cb);
+  }
+  exec0('ps -Ao pid,%cpu,%mem,comm |sort -nrk 2 | head -n 5 | awk \'{gsub("(.+/)","",$4);print "<"substr($4,1,13)"<" "," $1 "," $2 "," $3 ":" }\'',cb)
 })
 app.get('/crypto', function (req, res) {
   request.get('https://api.coinmarketcap.com/v1/ticker/',{},function(err,gres,body){
     if(err) return
     if(res.statusCode !== 200 ) return
     
-    qoutes=JSON.parse(body)
+    var qoutes=JSON.parse(body)
     qoutes.splice(5,qoutes.length-5)
     for (var i = 0; i < 5; i++) {
       qoutes[i] = [qoutes[i].id,qoutes[i].price_usd]
     }
     res.send(qoutes)
-  });
+  })
 })
 
 io.on('connection', function(socket){
-  nowplaying.on("playing", function (data) {
-    socket.broadcast.emit('playing', data);
-  });
-  nowplaying.on("paused", function (data) {
-    socket.broadcast.emit('paused', data);
-  });
+  nowplaying.on('playing', function (data) {
+    socket.broadcast.emit('playing', data)
+  })
+  nowplaying.on('paused', function (data) {
+    socket.broadcast.emit('paused', data)
+  })
   
-});
+})
 
 
 http.listen(3000, function(){
   console.log('running on http://localhost:3000')
-});
+})
--- public/js/lib/ajax.js
+++ public/js/lib/ajax.js
@@ -1,12 +1,14 @@
-function ajax(path,cb) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200) {
-      cb(null,this.responseText)
-    }else{
-      cb(true,null)
+define(function (require) {
+  return function ajax(path,cb) {
+    var xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function(){
+      if (this.readyState == 4 && this.status == 200) {
+        cb(null,this.responseText)
+      }else{
+        cb(true,null)
+      }
     }
+    xhttp.open('GET', path, true)
+    xhttp.send()
   }
-  xhttp.open("GET", path, true);
-  xhttp.send();
-}
+})
