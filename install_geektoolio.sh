aaabdd98120810ef4457af14fe2f161542ec2c41
Igor Susman <igor.susman@gmail.com>
2017-10-09 17:31:50 +0200

    add shebang exit on errors properly define current dir replace awk by sed replace sed delimiters

--- install_geektoolio.sh
+++ install_geektoolio.sh
@@ -1,29 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+user="$(id -u -nr)"
+
 echo "checking for dependencies..."
 
-if ! location="$(type -p "brew")" || [ -z "brew" ]; then
-	echo "installing brew"
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  else
-  	echo "brew is already installed. Proceeding..."
+if ! command -v brew 2>&1 > /dev/null; then
+  echo "installing brew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "brew is already installed. Proceeding..."
 fi
 
 echo "installing nowplaying dependencies"
 
-if ! exist="$(gem list bundler -i)" ;then
-	echo "installing bundler "
-	sudo gem install bundler
-  else
-  	echo "bundler is already installed. Proceeding..."
+if ! gem list bundler -i 2>&1 /dev/null; then
+  echo "installing bundler "
+  sudo gem install bundler
+else
+  echo "bundler is already installed. Proceeding..."
 fi
 
 echo "installing gem dependencies..."
 bundle install
 
-if ! location="$(type -p "node")" || [ -z "node" ]; then
-	echo "installing nodejs"
-	brew install nodejs
-  else
-  	echo "nodejs is already installed. Proceeding..."
+if ! command -v node 2>&1 > /dev/null; then
+  echo "installing nodejs"
+  brew install nodejs
+else
+  echo "nodejs is already installed. Proceeding..."
 fi
 
 echo "installing npm dependencies"
@@ -31,19 +37,19 @@ echo "installing npm dependencies"
 npm install
 
 while true; do
-    read -p "would you like me to start geektoolio server on startup?" yn
-    case $yn in
-        [Yy]* ) echo "setting up launcDeamond...";
-				cat com.liorhakim.geektoolio-template.plist | awk -v P="$(pwd)" -v U="$USER" '{gsub(/pwd/,P,$0);gsub(/user/,U,$0); print}' > com.liorhakim.geektoolio.plist
-				sudo cp com.liorhakim.geektoolio.plist /Library/LaunchDaemons/com.liorhakim.geektoolio.plist
-				sudo chown root:wheel /Library/LaunchDaemons/com.liorhakim.geektoolio.plist
-				sudo launchctl load  /Library/LaunchDaemons/com.liorhakim.geektoolio.plist
-				dir=$(pwd);echo "setenv PATH $PATH:$dir"|sudo tee -a /etc/launchd.conf
-				break;;
-        [Nn]* ) echo "bailing on autostart and proceeding..."
-				break;;
-        * ) echo "Please answer yes or no.";;
-    esac
+  read -p "would you like me to start geektoolio server on startup?" yn
+  case $yn in
+    [Yy]*) echo "setting up launcDeamond...";
+      sed -e "s~user~$user~;s~pwd~$dir~" < com.liorhakim.geektoolio-template.plist > com.liorhakim.geektoolio.plist
+      sudo cp com.liorhakim.geektoolio.plist /Library/LaunchDaemons/com.liorhakim.geektoolio.plist
+      sudo chown root:wheel /Library/LaunchDaemons/com.liorhakim.geektoolio.plist
+      sudo launchctl load  /Library/LaunchDaemons/com.liorhakim.geektoolio.plist
+      echo "setenv PATH $PATH:$dir" | sudo tee -a /etc/launchd.conf
+      ;;
+    [Nn]*) echo "bailing on autostart and proceeding..."
+      ;;
+    *) echo "Please answer yes or no.";;
+  esac
 done
 
 
